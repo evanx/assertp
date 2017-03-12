85bbffc44abbbc1d7da6c46ef4cbfa22596fbd34
Evan Summers <evan.summers@gmail.com>
2017-03-12 11:48:38 +0000

    publish

--- bin/test.js
+++ bin/test.js
@@ -2,22 +2,26 @@
 const asserta = require('../index.js');
 
 const object = {
-  f: () => true,
-  s: 'string'
+    f: () => true,
+    b: false,
+    s: 'string'
 }
 
 const errors = [];
 
-asserta.isFunction(object, 'f');
+asserta.ensureFunction(object, 'f');
 
 try {
-  asserta.isFunction(object, 's');
-  errors.push('s');
+    asserta.ensureFunction(object, 's');
+    errors.push('s');
 } catch (err) {
 }
 
 if (errors.length) {
-   console.error({errors});
-   process.exit(1);
+    console.error({errors});
+    process.exit(1);
 }
 
+if (process.env.NODE_ENV === 'development') {
+    console.log({t: typeof true, f: typeof false, u: typeof undefined, n: typeof null});
+}
--- index.js
+++ index.js
@@ -1,14 +1,57 @@
 
-module.exports = {
-    isFunction: (object, key) => {
+const ex = {
+    get: (object, key) => {
         if (typeof key !== 'string') {
             throw new Error(`Expected an string for property name`);
         }
         if (typeof object !== 'object') {
             throw new Error(`Expected an object for property '${key}'`);
         }
-        if (typeof object[key] !== 'function') {
-            throw new Error(`Expected a function for '${key}'`);
+        return object[key];
+    },
+    ensure: (object, key) => {
+        const value = ex.get(object, key);
+        if (value === undefined) {
+            throw new Error(`Expected property '${key}'`);
+        }
+        return value;
+    },
+    has: (object, key) => {
+        const value = ex.get(object, key);
+        if (value === undefined) {
+            return false;
+        }
+        return true;
+    },
+    hasType: (object, key, type) => {
+        if (typeof type !== 'string') {
+            throw new Error(`Expected a type specification for '${key}'`);
+        }
+        const value = ex.get(object, key);
+        if (value === undefined) {
+            return false;
+        }
+        if (typeof value !== type) {
+            throw new Error(`Expected type '${type}' for '${key}'`);
+        }
+        return true;
+    },
+    ensureBoolean: (object, key) => {
+        const value = ex.ensure(object, key);
+        if (typeof value !== 'boolean') {
+            throw new Error(`Expected boolean'${key}'`);
+        }
+        return value;
+    },
+    hasFunction: (object, key) => {
+        return ex.hasType(object, key, 'function');
+    },
+    ensureFunction: (object, key) => {
+        const value = ex.ensure(object, key);
+        if (typeof value !== 'function') {
+            throw new Error(`Expected boolean'${key}'`);
         }
     }
 }
+
+module.exports = ex;
